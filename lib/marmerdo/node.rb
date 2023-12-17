23ef6e573b2b534d91d536cd7336e45f913fede9
sijiaoh <sijiaoh@outlook.com>
2023-12-17 13:10:45 +0900

    Fix Node#name type.

--- lib/marmerdo/node.rb
+++ lib/marmerdo/node.rb
@@ -1,12 +1,13 @@
 module Marmerdo
   class Node
+    # @return [Symbol, nil]
     attr_reader :name
     # @return [Symbol, nil]
     attr_reader :namespace
     attr_accessor :relationships
 
     def initialize(name:, namespace:)
-      @name = name.to_sym
+      @name = name&.to_sym
       @namespace = namespace&.to_sym
     end
   end
